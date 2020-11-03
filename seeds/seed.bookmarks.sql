f0ffaaca56a3291cd90f4271f3d84818217d933e
Christopher <chrisobrien416@gmail.com>
2020-11-03 12:03:22 -0500

    before refactoring

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,9 @@
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+  ('first bookmark', 'cats.com', 'lotsa cat pics', 10),
+  ('second bookmark', 'dogs.com', 'lotsa dog pics', 10),
+  ('third bookmark', 'catsanddogs.com', 'lotsa cat and dog pics', 10),
+  ('fourth bookmark', 'dogsandcats.com', 'lotsa dog and cat pics', 10),
+  ('fifth bookmark', 'kitties.com', 'lotsa kitty pics', 10),
+  ('sixth bookmark', 'fatcats.com', 'lotsa fat cat pics', 10),
+  ('seventh bookmark', 'smallcats.com', 'lotsa small cat pics', 10)
